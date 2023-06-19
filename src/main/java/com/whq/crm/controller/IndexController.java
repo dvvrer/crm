769fda8e2730ddd7b466b4708f84fbcadf433e5b
dvvrer <124668498+dvvrer@users.noreply.github.com>
2023-06-19 22:06:03 +0800

    Initial commit

---
+++ src/main/java/com/whq/crm/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.whq.crm.controller;
+
+import com.whq.crm.base.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController extends BaseController {
+
+    /**
+     * 系统登录页
+     * @return
+     */
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+
+    //系统界面欢迎页
+    @RequestMapping("welcome")
+    public String welcome(){
+        return "welcome";
+    }
+
+    /**
+     * 后端管理主页面
+     * @return
+     */
+    @RequestMapping("main")
+    public String main(){
+        return "main";
+    }
+}
