3ae163780404b2cac8c482b1ca9c5f90bb8a0031
dvvrer <124668498+dvvrer@users.noreply.github.com>
2023-06-20 09:39:06 +0800

    add user file

---
+++ src/main/java/com/whq/crm/vo/User.java
@@ -0,0 +1,73 @@
+package com.whq.crm.vo;
+
+public class User {
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    private Integer age;
+
+    private String gender;
+
+    private String email;
+
+    private String trueName;
+
+    public String getTrueName() {
+        return trueName;
+    }
+
+    public void setTrueName(String trueName) {
+        this.trueName = trueName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender == null ? null : gender.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+}
