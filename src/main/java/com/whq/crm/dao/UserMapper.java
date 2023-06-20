331d7fab8c729c03f096741612ff4b5f4b30c635
dvvrer <124668498+dvvrer@users.noreply.github.com>
2023-06-20 09:13:39 +0800

    1

--- src/main/java/com/whq/crm/dao/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.whq.crm.dao;
-
-import com.whq.crm.base.BaseMapper;
-import com.whq.crm.vo.User;
-
-public interface UserMapper extends BaseMapper<User,Integer> {
-
-    public User queryUserByName(String userName);
-}
